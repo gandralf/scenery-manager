404cdb649a6613be2dc1306f58c1e183420be6d4
Alexandre Gandra Lages <gandra@gmail.com>
2011-07-22 00:41:01 -0400

    Better build 4

--- scenery-manager-app/dist.sh
+++ scenery-manager-app/dist.sh
@@ -1,12 +1,12 @@
-mvn clean test 
-DIST=scenery-app-1.0
-mkdir target/$DIST
+#!/bin/sh
+mvn clean test
 
-cd src/main/webapp
-cp -R . ../../../target/$DIST/
-cd -
-cp src/main/sh/* target/$DIST/
-cd target
-rm -rf `find $DIST -name .svn`
-tar cfvz $DIST.tar.gz $DIST
+if [[ $? == 0 ]] ; then
+    DIST="target/scenery-app-1.0"
+    mkdir "$DIST"
+    mkdir "$DIST/bin"
 
+    cp -R src/main/webapp $DIST
+    cp src/main/sh/* $DIST/bin
+    tar cfvz $DIST.tar.gz $DIST
+fi
--- scenery-manager-app/src/main/sh/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-CP="./WEB-INF/classes"
-for i in `ls ./WEB-INF/lib` ; do CP="$CP:./WEB-INF/lib/$i" ; done
-
-java -cp $CP br.com.devx.scenery.Main $*
-
