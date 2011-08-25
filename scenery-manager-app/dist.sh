520981290838b5b7ae9aed72c0d56da4898b37d4
Alexandre Gandra <gandra@gmail.com>
2011-08-25 17:22:28 -0300

    dist and install

--- scenery-manager-app/dist.sh
+++ scenery-manager-app/dist.sh
@@ -2,11 +2,14 @@
 mvn clean test
 
 if [[ $? == 0 ]] ; then
-    DIST="target/scenery-app-1.0"
-    mkdir "$DIST"
-    mkdir "$DIST/bin"
+    DIST="target/scenery-app"
+    mkdir -p "${DIST}/bin"
+    mkdir "${DIST}/ext"
 
-    cp -R src/main/webapp $DIST
-    cp src/main/sh/* $DIST/bin
-    tar cfvz $DIST.tar.gz $DIST
+    cp -R src/main/webapp ${DIST}
+    cp src/main/sh/* ${DIST}/bin
+    tar cfvz ${DIST}.tar.gz ${DIST}
+
+    sudo cp -R "${DIST}" /usr/local/
+    sudo ln -sf /usr/local/scenery-app/bin/scn /usr/local/bin/scn
 fi
