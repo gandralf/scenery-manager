7610b17dfb60a76354655f1bbad99dd9ad908e05
Alexandre Gandra Lages <gandra@gmail.com>
2012-04-14 10:28:16 -0400

    Cleaning: DSL

--- scenery-manager-app/src/test/java/br/com/devx/scenery/web/SceneryFilterTest.java
+++ scenery-manager-app/src/test/java/br/com/devx/scenery/web/SceneryFilterTest.java
@@ -24,47 +24,33 @@ public class SceneryFilterTest extends HttpUnitTestCase {
 
 
     public void testIndexHtml() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/" );
-        WebResponse response = m_client.getResponse(request);
-        assertTrue(response.getText().trim().contains("Hello, index.html!"));
+        pageShouldContains("/", "Hello, index.html!");
     }
 
     /**
      * Se o cenario estah mapeado, usa-o
      */
     public void testVelocity() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/velocity.do" );
-        WebResponse response = m_client.getResponse(request);
-        assertEquals("Hello, Zeh maneh", response.getText().trim());
+        pageShouldContains("/velocity.do", "Hello, Zeh maneh");
     }
 
     /**
      * Freemarker
      */
     public void testFreemarker() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/freemarker.do" );
-        WebResponse response = m_client.getResponse( request );
-        assertEquals("Hello, Zeh maneh", response.getText().trim());
+        pageShouldContains("/freemarker.do", "Hello, Zeh maneh");
     }
 
     public void testFreemarkerInclude() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/freemarker.do?test=include" );
-        WebResponse response = m_client.getResponse( request );
-        assertEquals("Hello, Zeh maneh", response.getText().trim());
+        pageShouldContains("/freemarker.do?test=include", "Hello, Zeh maneh");
     }
 
     public void testCustomTemplate() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/custom-template.do" );
-        WebResponse response = m_client.getResponse(request);
-        assertEquals("direct: Hello, Mr. Custom Template", response.getText().trim());
+        pageShouldContains("/custom-template.do", "direct: Hello, Mr. Custom Template");
     }
 
     public void testCustomTemplateEx() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/custom-template-ex.do" );
-        WebResponse response = m_client.getResponse(request);
-        String text = response.getText();
-        assertTrue(text.trim().contains("<html>"));
-        assertTrue(text.trim().contains("Mr. Custom Template"));
+        pageShouldContains("/custom-template-ex.do", "<html>", "Mr. Custom Template");
     }
 
     /**
@@ -72,98 +58,88 @@ public class SceneryFilterTest extends HttpUnitTestCase {
      */
     public void testRedirect() throws IOException, SAXException {
         m_app.setUrl("http://gandralf.github.com");
-
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/scenery-manager" );
-        WebResponse response = m_client.getResponse( request );
-
-        assertTrue(response.getText().toLowerCase().contains("scenery manager"));
+        pageShouldContains("/scenery-manager", "scenery manager");
     }
 
-    /**
-     * senao, aciona o outro site
-     */
     public void testError() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/velocity.do?error=true" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("<pre>     \"I'll forget a comma here\"</pre>"));
+        pageShouldContains("/velocity.do?error=true", "<pre>     \"I'll forget a comma here\"</pre>");
     }
 
     public void testVelocityMacro() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/velocityx.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("<td bgcolor=\"blue\">hello</td>"));
+        pageShouldContains("/velocityx.do", "<td bgcolor=\"blue\">hello</td>");
     }
 
     public void testVelocityParse() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/velocityx.do?test=parse" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("<td bgcolor=\"blue\">hello</td>"));
+        pageShouldContains("/velocityx.do?test=parse", "<td bgcolor=\"blue\">hello</td>");
     }
 
     public void testVelocityTools() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/tool.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("He didn't say, \\\"Stop!\\\""));
-        assertTrue(text.contains("&quot;bread&quot; &amp; &quot;butter&quot;"));
-        assertTrue(text.contains("hello+here+%26+there"));
+        pageShouldContains("/tool.do",
+                "He didn't say, \\\"Stop!\\\"",
+                "&quot;bread&quot; &amp; &quot;butter&quot;",
+                "hello+here+%26+there");
     }
 
     public void testWebInfClasses() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/classloader.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("Hello, Me!, your id is 1"));
+        pageShouldContains("/classloader.do", "Hello, Me!, your id is 1");
     }
 
     public void testWebInfLib() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/classloader.do?from=lib" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("Hello, Me!, your id is 1"));
+        pageShouldContains("/classloader.do?from=lib", "Hello, Me!, your id is 1");
     }
 
     public void testSitemesh() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/sitemesh.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue("Didn't load hello.ftl (body)", text.contains("Hello, Zeh maneh!"));
-        assertTrue("Didn't load main.flt decorator", text.contains("<title>Hello, sitemesh!</title>"));
-        assertTrue("Didn't load main.flt decorator", text.contains("Welcome to the world"));
+        pageShouldContains("/sitemesh.do",
+                "Hello, Zeh maneh!", // In hello.ftl (body)
+                "<title>Hello, sitemesh!</title>", // In main.ftl decorator
+                "Welcome to the world");
     }
 
     public void testVMSitemesh() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/vmsitemesh.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue("Didn't load hello.vm (body)", text.contains("Hello, Zeh maneh!"));
-        assertTrue("Didn't load main.vm decorator", text.contains("<title>Hello, sitemesh!</title>"));
-        assertTrue("Didn't load main.vm decorator", text.contains("Welcome to the world"));
-        assertTrue("Didn't load hello.vm (body)", text.contains("This is velocity!"));
+        pageShouldContains("/vmsitemesh.do",
+                "Hello, Zeh maneh!", // body at hello.vm
+                "<title>Hello, sitemesh!</title>", // decorator at main.vm
+                "Welcome to the world", // decorator at main.vm
+                "This is velocity!"); // body at hello.vm
     }
 
     public void testHeadBody() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/vmsitemesh-head-body.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue("Didn't load hello.vm (body)", text.contains("Hello, Zeh maneh!"));
-        assertTrue("Didn't load main.vm decorator", text.contains("<title>Hello, sitemesh!</title>"));
-        assertTrue("Didn't load main.vm decorator", text.contains("Welcome to the world"));
-        
+        String text = pageContentInLowerCase("/vmsitemesh-head-body.do");
+        assertTextContains(text,
+                "Hello, Zeh maneh!", // body at hello.vm
+                "<title>Hello, sitemesh!</title>", // main.vm decorator
+                "Welcome to the world");  // main.vm decorator
+        assertTextHasNoDuplicatedTags(text);
+    }
+
+    private void assertTextHasNoDuplicatedTags(String text) {
         assertEquals("Double html", -1, text.indexOf("<html>", text.indexOf("<html>") + 1));
         assertEquals("Double head", -1, text.indexOf("<head>", text.indexOf("<head>") + 1));
         assertEquals("Double body", -1, text.indexOf("<body>", text.indexOf("<body>") + 1));
     }
 
     public void testSitemeshPrint() throws IOException, SAXException {
-        WebRequest request   = new GetMethodWebRequest( "http://localhost/sitemesh-print.do" );
-        WebResponse response = m_client.getResponse( request );
-        String text = response.getText();
-        assertTrue(text.contains("Hello, Zeh maneh!"));
-        assertTrue("Didn't load main.flt decorator", text.contains("<title>Hello, sitemesh!</title>"));
-        assertFalse("Didn't load main.flt decorator", text.contains("Welcome to the world"));
+        String text = pageContentInLowerCase("/sitemesh-print.do");
+        assertTextContains(text, "Hello, Zeh maneh!", "<title>Hello, sitemesh!</title>");
+        assertFalse(text.contains("Welcome to the world"));
+    }
+
+    private void pageShouldContains(String uri, String ... expectedTexts) throws IOException, SAXException {
+        String text = pageContentInLowerCase(uri);
+        assertTextContains(text, expectedTexts);
+    }
+
+    private String pageContentInLowerCase(String uri) throws IOException, SAXException {
+        WebRequest request   = new GetMethodWebRequest( "http://localhost" + uri );
+        WebResponse response = m_client.getResponse(request);
+        
+        return response.getText().trim().toLowerCase();
+    }
+
+    private void assertTextContains(String text, String ... expectedTexts) {
+        for (String expectedText: expectedTexts) {
+            String expected = expectedText.toLowerCase();
+            assertTrue("\"" + text + "\"\n does not contains " + expected, text.contains(expected));
+        }
     }
 }
